9999f8293b0e033f62e932389351383fd6310cbe
devmatteini <dev.matteini@gmail.com>
2021-03-11 22:19:09 +0100

    add panel with summary of features

--- src/pages/simple/simple-form.tsx
+++ src/pages/simple/simple-form.tsx
@@ -22,51 +22,60 @@ const SimpleForm: React.FC = () => {
     const onSubmit = handleSubmit((data) => setResult(data))
 
     return (
-        <div className="container w-50">
-            <Form onSubmit={onSubmit} className="m-3" autoComplete="off">
-                <div className="d-flex flex-column" style={{ rowGap: "1rem" }}>
-                    <div>
-                        <label>Nome</label>
-                        <Form.Control name="name" isInvalid={!!errors.name} ref={register({ required: true })} />
-                        {errors.name && <small className="text-danger">Campo obbligatorio</small>}
+        <div className="mx-5 row">
+            <div className="col-8">
+                <Form onSubmit={onSubmit} className="m-3" autoComplete="off">
+                    <div className="d-flex flex-column" style={{ rowGap: "1rem" }}>
+                        <div>
+                            <label>Nome</label>
+                            <Form.Control name="name" isInvalid={!!errors.name} ref={register({ required: true })} />
+                            {errors.name && <small className="text-danger">Campo obbligatorio</small>}
+                        </div>
+
+                        <div>
+                            <label>Squadra</label>
+                            <Form.Control
+                                as="select"
+                                name="team"
+                                isInvalid={!!errors.team}
+                                ref={register({ required: true })}
+                            >
+                                {teamOptions.map((x) => (
+                                    <option key={x.value} value={x.value}>
+                                        {x.label}
+                                    </option>
+                                ))}
+                            </Form.Control>
+                            {errors.team && <small className="text-danger">Campo obbligatorio</small>}
+                        </div>
+
+                        <div>
+                            <label>Posizione</label>
+                            <Form.Control
+                                name="position"
+                                isInvalid={!!errors.position}
+                                ref={register({ pattern: positionRegex })}
+                            />
+                            {errors.position && <small className="text-danger">Posizione non valida</small>}
+                        </div>
                     </div>
 
-                    <div>
-                        <label>Squadra</label>
-                        <Form.Control
-                            as="select"
-                            name="team"
-                            isInvalid={!!errors.team}
-                            ref={register({ required: true })}
-                        >
-                            {teamOptions.map((x) => (
-                                <option key={x.value} value={x.value}>
-                                    {x.label}
-                                </option>
-                            ))}
-                        </Form.Control>
-                        {errors.team && <small className="text-danger">Campo obbligatorio</small>}
+                    <div className="mt-3 d-flex justify-content-between">
+                        <Button variant="primary" type="submit">
+                            Invia
+                        </Button>
                     </div>
+                </Form>
 
-                    <div>
-                        <label>Posizione</label>
-                        <Form.Control
-                            name="position"
-                            isInvalid={!!errors.position}
-                            ref={register({ pattern: positionRegex })}
-                        />
-                        {errors.position && <small className="text-danger">Posizione non valida</small>}
-                    </div>
-                </div>
+                {result && <p className="mt-3 alert alert-success">{JSON.stringify(result)}</p>}
+            </div>
 
-                <div className="mt-3 d-flex justify-content-between">
-                    <Button variant="primary" type="submit">
-                        Invia
-                    </Button>
-                </div>
-            </Form>
-
-            {result && <p className="mt-3 alert alert-success">{JSON.stringify(result)}</p>}
+            <div className="col m-3 alert alert-info">
+                <p className="mb-1">- Required and optional fields</p>
+                <p className="mb-1">- Built-in validation</p>
+                <p className="mb-1">- Errors</p>
+                <p className="mb-1">- Typescript schema type</p>
+            </div>
         </div>
     )
 }
